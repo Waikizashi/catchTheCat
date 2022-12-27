3328695be3a183bb8d14a02301849bfe3a273c0e
Eugene <XandrGrune@yandex.ru>
2022-12-27 20:54:58 +0100

    menu_upd

--- public_html/src/js/hiScreen.js
+++ public_html/src/js/hiScreen.js
@@ -1,18 +1,27 @@
 const hiScreen = document.querySelector('.hiScreen')
+const welcome = document.querySelector('#welcome')
 const pause = document.querySelector('#pause')
 const reboot = document.querySelector('#reboot')
 const start = document.querySelector('#start')
-const hiScrEls = document.querySelectorAll('.hiScreen p')
+const settings = document.querySelector('#settings')
+const lvls = document.querySelectorAll('#choseMode li p')
 
-
+lvls.forEach(el =>{
+    el.addEventListener('click', ()=>{
+        lvls.forEach(el =>{
+            el.classList.remove('chosen')
+        })
+        el.classList.add('chosen')
+    })
+})
 
 
 start.addEventListener('click', ()=>{
-    hiScreen.classList.add('closeHiScreen')
-    hiScrEls.array.forEach(el => {
-        el.style = 'color: balck'
-    });
-    //hiScreen.style = `transform: translateY(${(window.innerHeight-340)/2}px)`
+    hiScreen.classList.add('close')
+    settings.classList.add('close')
+    welcome.classList.add('close')
+    start.classList.add('close')
+    hiScreen.style = `transform: translateY(${(window.innerHeight-340)/2}px)`
     pause.innerHTML = 'pause'
     reboot.innerHTML = 'reboot'
     //_init_()
@@ -20,7 +29,10 @@ start.addEventListener('click', ()=>{
 
 
 pause.addEventListener('click', ()=>{
-    hiScreen.classList.remove('closeHiScreen')
+    hiScreen.classList.remove('close')
+    settings.classList.remove('close')
+    welcome.classList.remove('close')
+    start.classList.remove('close')
     hiScreen.style = ``
     pause.innerHTML = '...'
 })
