550c7a9d5575a20a563b58d9a447dc8920912f78
Eugene <XandrGrune@yandex.ru>
2022-12-29 02:29:36 +0100

    full visual set

--- src/components/Layout/main-area/controls/Menu.js
+++ src/components/Layout/main-area/controls/Menu.js
@@ -1,17 +1,26 @@
 import cn from  'classnames';
+import { useContext } from 'react';
+import { StateContext } from '../../../../context/stateContext';
 
 import s from './Menu.module.css';
 
 function Menu() {
+  const {onSetHisState, hiScreenState} = useContext(StateContext)
+
+  const onPause = ()=>{
+    console.log("####: ", hiScreenState)
+    onSetHisState()
+  }
+
   return (
     <div className={cn(s.menu)}>
 
       
-    <div className={cn(s.btn)}><p>fuck somebody</p></div>
+    <div className={cn(s.btn)}><div>fuck somebody</div></div>
 
       {/* <div className={cn(s.mainBtns)}> */}
-        <div className={cn(s.btn)}><p>reboot</p></div>
-        <div className={cn(s.btn)}><p>pause</p></div>
+        <div className={cn(s.btn)}><div>reboot</div></div>
+        <div className={cn(s.btn)} onClick={onPause}><div>{hiScreenState ? 'pause' : 'start'}</div></div>
       {/* </div> */}
 
     </div>
--- src/components/Layout/main-area/play-area/hiScreen/HiScreen.js
+++ src/components/Layout/main-area/play-area/hiScreen/HiScreen.js
@@ -1,24 +1,80 @@
 import cn from  'classnames';
+import React, { useContext, useState } from "react";
+import { StateContext } from '../../../../../context/stateContext';
 
 import s from './hiScreen.module.css';
 
+function isInteger(value) {
+  return typeof value === "number" && value % 1 === 0;
+}
+
+
 function HiScreen() {
+
+  
+
+  const {onSetHisState, hiScreenState, onSetMode, gameMode} = useContext(StateContext)
+
+  const [modifier, setModifier] = useState(0)
+  
+
+  const clickToStart = () => {
+
+    onSetHisState()
+  };
+
+  function handleChange(event){
+    console.log('Modifier value: ',event.target.value)
+    if(isInteger(event.target.value)){
+      setModifier(0)
+    }
+    else if(event.target.value >99 ){
+      setModifier(99)
+    }
+    else if(event.target.value <0 ){
+      setModifier(0)
+    }
+    else{
+      setModifier(event.target.value)
+    }
+  }
+
+  function onChoseMode(event){
+      console.log(event.target.id)
+      onSetMode(event.target.id)
+  }
+
+
   return (
-    <div class={s.hiScreen}>
+    <div className={cn(s.hiScreen,{
+      [s.closeModal]: !hiScreenState,
+      [s.openModal]: hiScreenState
+    })}>
             <p className={s.welcome}>Welcome to "Catch tha cat" game</p>
-            <div className={s.settings}>
+            <div className={cn(s.settings)
+            }>
                 <p className={s.p}>Choose game mode:</p>
                 <ul className={s.choseMode}>
-                    <li><p id='veryEasy' className={cn(s.menuBtn)}>very_easy</p></li>
-                    <li><p id='easy' className={cn(s.menuBtn)}>easy</p></li>
-                    <li><p id='medium' className={cn(s.menuBtn)}>medium</p></li>
-                    <li><p id='hard' className={cn(s.menuBtn)}>hard</p></li>
-                    <li><p id='Extreme' className={cn(s.menuBtn)}>Extreme</p></li>
+                    <li><p id='veryEasy' className={cn(s.menuBtn,{
+                      [s.chosen]: gameMode === 'veryEasy'
+                    })} onClick={onChoseMode}>very_easy</p></li>
+                    <li><p id='easy' className={cn(s.menuBtn,{
+                      [s.chosen]: gameMode === 'easy'
+                    })} onClick={onChoseMode}>easy</p></li>
+                    <li><p id='medium' className={cn(s.menuBtn,{
+                      [s.chosen]: gameMode === 'medium'
+                    })} onClick={onChoseMode}>medium</p></li>
+                    <li><p id='hard' className={cn(s.menuBtn,{
+                      [s.chosen]: gameMode === 'hard'
+                    })} onClick={onChoseMode}>hard</p></li>
+                    <li><p id='Extreme' className={cn(s.menuBtn,{
+                      [s.chosen]: gameMode === 'Extreme'
+                    })} onClick={onChoseMode}>Extreme</p></li>
                 </ul>
                 <p className={s.p} >set the modifier (optional):</p>
-                <input className={s.extraOption} value="0" min="0" max="99" type="number"/>
+                <input className={s.extraOption} value={modifier} onChange={handleChange} type="number"/>
             </div>
-            <p className={s.start} > {'<<'} click here to start game {'>>'} </p>
+            <p className={s.start} onClick={clickToStart}> {'<<'} click here to start game {'>>'} </p>
     </div>
   );
 }
